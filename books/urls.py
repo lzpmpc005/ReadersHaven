921b4e2678b1222bfb2b0ab6317c240b80c49b08
carimara <karimarabakh@gmail.com>
2023-11-09 03:52:14 +0500

    Removed REST Framework, added search by title

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import Author_list, Book_list, BookFilterByAuthor
+from .views import book_list, create_author, create_book, filter_books_by_author
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name = 'books-book_list'),
-    path('booklist', Book_list.as_view(), name = 'BookList'),
-    path('authorlist', Author_list.as_view(), name = 'AuthorList'),
-    path('booklist/filter/<int:author_id>/', BookFilterByAuthor.as_view(), name = 'Filter-by-Author')
+    path('booklist/', book_list, name = 'BookList'),
+    path('create_author', create_author, name = 'Create_Author'),
+    path('create_book', create_book, name = 'Create_Book'),
+    path('booklist/filter/<int:author_id>/', filter_books_by_author, name = 'Filter_By_Author')
 ]
--- books/views.py
+++ books/views.py
@@ -1,24 +1,49 @@
 from django.views import generic
 from .models import Book, Author
-from rest_framework import generics
-from .serializers import AuthorSerializer, BookSerializer
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 class BookListView(generic.ListView):
     model = Book
     context_object_name = 'book_list'
 
-class Author_list(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
+def book_list(request):
+    title = request.GET.get('title')
+    books = Book.objects.all()
+    if title:
+        books = books.filter(title__icontains = title)
+    book_array = [{'id': book.id,
+                   'title': book.title,
+                   'author': book.author.author_name,
+                   'price': str(book.price) + "$"
+                   } for book in books]
+    return JsonResponse(book_array, safe = False)
 
-class Book_list(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+@csrf_exempt
+def create_author(request):
+    if request.method == 'POST':
+        author_name = json.loads(request.body)["name"]
+        author = Author.objects.create(author_name = author_name)
+        return JsonResponse({'author_id': author.id})
 
-class BookFilterByAuthor(generics.ListAPIView):
-    serializer_class = BookSerializer
-    
-    def get_queryset(self):
-        author_id = self.kwargs["author_id"]
-        return Book.objects.filter(author__id = author_id)
+@csrf_exempt   
+def create_book(request):
+    if request.method == 'POST':
+        json_request = json.loads(request.body)
+        title = json_request['title']
+        author_id = json_request['author_id']
+        price = json_request['price']
+        author = Author.objects.get(id = author_id)
+        new_book = Book.objects.create(title = title, price = price, author = author)
+        return JsonResponse({'book_id': new_book.id})
 
+def filter_books_by_author(request, author_id):
+    author = Author.objects.get(id = author_id)
+    books = Book.objects.filter(author = author)
+    book_array = [{'id': book.id,
+                   'title': book.title,
+                   'author': book.author.author_name,
+                   'price': str(book.price) + "$"
+                   } for book in books]
+    return JsonResponse(book_array, safe = False)
