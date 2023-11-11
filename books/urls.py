d6fd7507fe9937db9071a3edc3b8bd4d552b9ac6
Hongtao Li <hongtaolee@hotmail.com>
2023-11-11 17:01:14 +0100

    modified:   books/__pycache__/urls.cpython-311.pyc 	modified:   books/__pycache__/views.cpython-311.pyc 	modified:   books/urls.py 	modified:   books/views.py

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import book_list, create_author, create_book, filter_books_by_author,filter_books_by_author_name
+from .views import book_list, create_author, create_book, filter_books_by_author,filter_books_by_author_name,search_books_by_title
 
 urlpatterns = [
     path('booklist/', book_list, name = 'BookList'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create_book', create_book, name = 'Create_Book'),
     path('booklist/filter/<int:author_id>/', filter_books_by_author, name = 'Filter_By_Author'),
     path('booklist/filter/<author_name>/', filter_books_by_author_name, name = 'Filter_By_Author_name'),
+    path('booklist/search/<search_title>/', search_books_by_title, name = 'search_books_by_title'),
 ]
