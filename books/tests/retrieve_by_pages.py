a1c93ccc445bc65ec6979c6ddbd84fcecd494066
Hongtao Li <hongtaolee@hotmail.com>
2023-11-28 11:20:28 +0100

    don't know why

--- books/tests/retrieve_by_pages.py
+++ books/tests/retrieve_by_pages.py
@@ -2,8 +2,8 @@ import requests
 
 url = "http://localhost:8000/books/booklist/"
 data = {
-    "page": 1,
-    "num_per_page": 20,
+    "page": 10000,
+    "num_per_page": 10,
 }
 response = requests.get(url, params=data)
 print(response.json())
